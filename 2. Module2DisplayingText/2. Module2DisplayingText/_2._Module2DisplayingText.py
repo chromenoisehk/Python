36072bef9fb6042ca2381e3dd2bda0eff368ecb8
chromenoisehk <chromenoisehk@gmail.com>
2015-03-10 18:41:13 +0900

    2. Module 2 Displaying Text

---
+++ 2. Module2DisplayingText/2. Module2DisplayingText/_2._Module2DisplayingText.py
@@ -0,0 +1,17 @@
+print('The capybara is the worlds largest rodent') # 작은 따음표 사용
+print("The capybara likes to live in groups")       # 큰 따음표 사용
+print('The "capybara" can swim')                    # 작은 따음표 안의 큰 따음표
+print("And, This is 'text'")                            # 큰 따음표 안의 작은 따음표
+
+
+print("the capybara lives in \nSouth America")      # \n=Enter키와 같은 효과
+
+print("""This is the the strangest way 
+to print over multiple lines I Know""")             # 삼중 따음표 = 끊어져 있으면 자동 \n. #Python에만 있는 기능
+
+print('here is a double quote "' + " here is a single quote '") # 큰따음표와 작은따음표가 함깨 있을때의 코딩법
+print("or you can just do this \" does that work")
+print("can I just print a \ on the screen?")
+
+#만약 \news라고 보여주고 싶다면?
+print("But what if I Want \\news ")                 # \n 앞에 \를 붙여야 한다.
